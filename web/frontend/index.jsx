ae40692ad1d4060920849dde0ad8eb3b3bc4aa22
Seymur Rahimov <noiseymur@hotmail.com>
2023-10-17 11:04:33 +0400

    Updated root render function

--- web/frontend/index.jsx
+++ web/frontend/index.jsx
@@ -1,9 +1,10 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { initI18n } from "./utils/i18nUtils";
 
 // Ensure that locales are loaded before rendering the app
 initI18n().then(() => {
-  ReactDOM.render(<App />, document.getElementById("app"));
+  const root = ReactDOM.createRoot(document.getElementById("app"));
+  root.render(<App />);
 });
